0a75369148efe91ceb6c9a2c54641fd5023630ba
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2018-10-04 20:00:49 +0300

    add signbook and view book

--- guestbook/views.py
+++ guestbook/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Comment
+from .forms import CommentForm
 
 # Create your views here.
+def index(request):
+    comments = Comment.objects.order_by('-date_added')
+    context = {'comments': comments}
+    return render(request, 'guestbook/index.html', context)
+
+def sign(request):
+    form = CommentForm()
+
+    context = {'form' : form}
+    return render(request, 'guestbook/sign.html', context)
