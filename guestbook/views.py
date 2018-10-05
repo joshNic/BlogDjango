1f411cb4f184bd34c97ae34ded81c2fae213b066
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2018-10-05 18:03:08 +0300

    add guestbook sign to database

--- guestbook/views.py
+++ guestbook/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Comment
 from .forms import CommentForm
 
@@ -9,7 +9,13 @@ def index(request):
     return render(request, 'guestbook/index.html', context)
 
 def sign(request):
-    form = CommentForm()
-
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            new_comment = Comment(name=request.POST['name'], comment=request.POST['comment'])
+            new_comment.save()
+            return redirect('index')
+    else:
+        form = CommentForm()
     context = {'form' : form}
     return render(request, 'guestbook/sign.html', context)
